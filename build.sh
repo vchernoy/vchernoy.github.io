d41522275a9653fe34f7b87bd4eccf478f602e8d
Slava Chernoy <vchernoy@gmail.com>
2026-02-06 16:59:36 -0800

    fix 2

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 
 rm -rf public/ publication_types/ resources/ tags/ sitemap.xml post/ js/ index.html index.xml img/ home/ css/ 404.html '=' styles.css categories/
+rm -rf webfonts/
+
 hugo
 cp -r public/* ./
+
 
